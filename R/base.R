3c0339ce8645fbee5afda6c5251030a51051cbe6
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-09-09 18:29:59 +0200

    update BaseRegressor

--- R/base.R
+++ R/base.R
@@ -3,29 +3,108 @@
 # 1 - BaseRegressor -----------------------------------------------------------
 
 BaseRegressor <- R6::R6Class("BaseRegressor",
-                         public = list(
-                           name = "BaseRegressor",
-                           type = "regression",
-                           model = NULL,
-                           initialize = function(name = "BaseRegressor",
-                                                 type = "regression",
-                                                 model = NULL) {
-                             self$name <- name
-                             self$type <- type
-                             self$model <- model
-                           },
-                           get_name = function() {
-                             self$name
-                           },
-                           get_type = function() {
-                             self$type
-                           },
-                           get_model = function() {
-                             self$model
-                           },
-                           set_model = function(model) {
-                             self$model <- model
-                           }))
+                             public = list(
+                               name = "BaseRegressor",
+                               type = "regression",
+                               model = NULL,
+                               X_train = NULL,
+                               y_train = NULL,
+                               initialize = function(name = "BaseRegressor",
+                                                     type = "regression",
+                                                     model = NULL,
+                                                     X_train = NULL,
+                                                     y_train = NULL) {
+                                 self$name <- name
+                                 self$type <- type
+                                 self$model <- model
+                                 self$X_train <- X_train
+                                 self$y_train <- y_train
+                               },
+                               get_name = function() {
+                                 self$name
+                               },
+                               get_type = function() {
+                                 self$type
+                               },
+                               get_model = function() {
+                                 self$model
+                               },
+                               set_model = function(model) {
+                                 self$model <- model
+                               },
+                               fit = function(X, y, ...) {
+                                 self$X_train <- X
+                                 self$y_train <- y
+                                 self$set_model(stats::.lm.fit(x = self$X_train,
+                                                               y = self$y_train,
+                                                               ...))
+                               },
+                               predict = function(X, level = NULL,
+                                                  method = c("splitconformal",
+                                                             "other"),
+                                                  ...) {
+                                 if (is.null(level))
+                                 {
+
+                                   return(drop(X%*%self$model$coefficients))
+
+                                 } else { # prediction interval
+
+                                   method <- match.arg(method)
+
+                                   if (identical(method, "splitconformal"))
+                                   {
+                                     # see https://github.com/ryantibs/conformal
+                                     # see https://github.com/ryantibs/conformal
+                                     # see https://github.com/ryantibs/conformal
+                                     stopifnot(is_wholenumber(level))
+                                     stopifnot(level > 0 && level < 100)
+                                     stopifnot(!is.null(self$X_train))
+                                     stopifnot(!is.null(self$y_train))
+
+                                     idx_train_calibration <- split_data(self$y_train,
+                                                                         p = 0.5,
+                                                                         seed = 123)
+
+                                     X_train_sc <- self$X_train[idx_train_calibration,]
+                                     y_train_sc <- self$y_train[idx_train_calibration]
+                                     X_calibration_sc <- self$X_train[-idx_train_calibration,]
+                                     y_calibration_sc <- self$y_train[-idx_train_calibration]
+
+                                     fit_obj_train_sc <- .lm.fit(X_train_sc, y_train_sc)
+                                     y_pred_calibration <- X_calibration_sc%*%fit_obj_train_sc$coefficients
+                                     abs_residuals <- abs(y_calibration_sc - y_pred_calibration)
+
+                                     # quantile_absolute_residuals_lower <- quantile_scp(abs_residuals,
+                                     #                                                   alpha = (1 - level / 100) / 2)
+                                     # quantile_absolute_residuals_upper <- quantile_scp(abs_residuals,
+                                     #                                                   alpha = 1 - (1 - level / 100) / 2)
+
+                                     # quantile_absolute_residuals_lower <- quantile(abs_residuals,
+                                     #                                               probs = (1 - level / 100) / 2)
+                                     # quantile_absolute_residuals_upper <- quantile(abs_residuals,
+                                     #                                               probs = 1 - (1 - level / 100) / 2)
+
+                                     quantile_absolute_residuals_lower <- quantile_scp(abs_residuals,
+                                                                                       alpha = (1 - level / 100))
+                                     quantile_absolute_residuals_upper <- quantile_scp(abs_residuals,
+                                                                                       alpha = (1 - level / 100))
+
+                                     preds <- drop(X%*%self$model$coefficients)
+
+                                     return(list(preds = preds,
+                                                 lower = preds - quantile_absolute_residuals_lower,
+                                                 upper = preds + quantile_absolute_residuals_upper))
+                                   }
+
+                                   if (identical(method, "other"))
+                                   {
+
+                                   }
+
+                                 }
+                               }))
+
 
 # 2 - BaseClassifier -----------------------------------------------------------
 
---
+++ R/utils.R
@@ -0,0 +1,133 @@
+
+# Check is whole number
+is_wholenumber <- function(x, tol = .Machine$double.eps^0.5)
+{
+  abs(x - round(x)) < tol
+}
+
+
+# Quantile split conformal prediction -----
+quantile_scp <- function(abs_residuals, alpha)
+{
+  n_cal_points <- length(abs_residuals)
+  k <- ceiling((0.5*n_cal_points + 1)*(1 - alpha))
+  return(rank(abs_residuals)[k])
+}
+
+# Split a dataset -----
+split_data <- function(y, p = 0.5, seed = 123)
+{
+  # from caret::createFolds
+  set.seed(seed)
+  stopifnot((p > 0) && (p < 1))
+  k <- floor(1/p)
+  return_list <- TRUE
+  returnTrain <- FALSE
+
+  if (inherits(y, "Surv"))
+    y <- y[, "time"]
+  if (is.numeric(y)) {
+    cuts <- floor(length(y)/k)
+    if (cuts < 2)
+      cuts <- 2
+    if (cuts > 5)
+      cuts <- 5
+    breaks <- unique(quantile(y, probs = seq(0, 1, length = cuts)))
+    y <- cut(y, breaks, include.lowest = TRUE)
+  }
+  if (k < length(y)) {
+    y <- factor(as.character(y))
+    numInClass <- table(y)
+    foldVector <- vector(mode = "integer", length(y))
+    for (i in 1:length(numInClass)) {
+      min_reps <- numInClass[i]%/%k
+      if (min_reps > 0) {
+        spares <- numInClass[i]%%k
+        seqVector <- rep(1:k, min_reps)
+        if (spares > 0)
+          seqVector <- c(seqVector, sample(1:k, spares))
+        foldVector[which(y == names(numInClass)[i])] <- sample(seqVector)
+      }
+      else {
+        foldVector[which(y == names(numInClass)[i])] <- sample(1:k,
+                                                               size = numInClass[i])
+      }
+    }
+  }
+  else {
+    foldVector <- seq(along = y)
+  }
+
+  out <- split(seq(along = y), foldVector)
+
+  return(out[[1]])
+
+}
+
+# Stratify stuff -----
+# from https://gist.github.com/mrdwab/6424112
+# stratified <- function(df, group, size, select = NULL,
+#                        replace = FALSE, bothSets = FALSE) {
+#   if (is.null(select)) {
+#     df <- df
+#   } else {
+#     if (is.null(names(select))) stop("'select' must be a named list")
+#     if (!all(names(select) %in% names(df)))
+#       stop("Please verify your 'select' argument")
+#     temp <- sapply(names(select),
+#                    function(x) df[[x]] %in% select[[x]])
+#     df <- df[rowSums(temp) == length(select), ]
+#   }
+#   df.interaction <- interaction(df[group], drop = TRUE)
+#   df.table <- table(df.interaction)
+#   df.split <- split(df, df.interaction)
+#   if (length(size) > 1) {
+#     if (length(size) != length(df.split))
+#       stop("Number of groups is ", length(df.split),
+#            " but number of sizes supplied is ", length(size))
+#     if (is.null(names(size))) {
+#       n <- setNames(size, names(df.split))
+#       message(sQuote("size"), " vector entered as:\n\nsize = structure(c(",
+#               paste(n, collapse = ", "), "),\n.Names = c(",
+#               paste(shQuote(names(n)), collapse = ", "), ")) \n\n")
+#     } else {
+#       ifelse(all(names(size) %in% names(df.split)),
+#              n <- size[names(df.split)],
+#              stop("Named vector supplied with names ",
+#                   paste(names(size), collapse = ", "),
+#                   "\n but the names for the group levels are ",
+#                   paste(names(df.split), collapse = ", ")))
+#     }
+#   } else if (size < 1) {
+#     n <- round(df.table * size, digits = 0)
+#   } else if (size >= 1) {
+#     if (all(df.table >= size) || isTRUE(replace)) {
+#       n <- setNames(rep(size, length.out = length(df.split)),
+#                     names(df.split))
+#     } else {
+#       # message(
+#       #   "Some groups\n---",
+#       #   paste(names(df.table[df.table < size]), collapse = ", "),
+#       #   "---\ncontain fewer observations",
+#       #   " than desired number of samples.\n",
+#       #   "All observations have been returned from those groups.")
+#       n <- c(sapply(df.table[df.table >= size], function(x) x = size),
+#              df.table[df.table < size])
+#     }
+#   }
+#   temp <- lapply(
+#     names(df.split),
+#     function(x) df.split[[x]][sample(df.table[x],
+#                                      n[x], replace = replace), ])
+#   set1 <- do.call("rbind", temp)
+#
+#   if (isTRUE(bothSets)) {
+#     set2 <- df[!rownames(df) %in% rownames(set1), ]
+#     list(SET1 = set1, SET2 = set2)
+#   } else {
+#     set1
+#   }
+# }
+#
+#
+
