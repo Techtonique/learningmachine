22caef312ee72a5d21e5b2467aa4e721622d0aff
T. Moudiki <thierry.moudiki@gmail.com>
2023-09-21 19:16:30 +0000

    predict for Base and Ranger Pt.1

--- R/base.R
+++ R/base.R
@@ -9,16 +9,22 @@ BaseRegressor <- R6::R6Class("BaseRegressor",
                                model = NULL,
                                X_train = NULL,
                                y_train = NULL,
+                               engine = NULL, 
+                               params = NULL,
                                initialize = function(name = "BaseRegressor",
                                                      type = "regression",
                                                      model = NULL,
                                                      X_train = NULL,
-                                                     y_train = NULL) {
+                                                     y_train = NULL,
+                                                     engine = NULL, 
+                                                     params = NULL) {
                                  self$name <- name
                                  self$type <- type
                                  self$model <- model
                                  self$X_train <- X_train
                                  self$y_train <- y_train
+                                 self$engine <- engine
+                                 self$params <- params
                                },
                                get_name = function() {
                                  self$name
@@ -32,32 +38,48 @@ BaseRegressor <- R6::R6Class("BaseRegressor",
                                set_model = function(model) {
                                  self$model <- model
                                },
+                               set_engine = function(engine) {
+                                 self$engine <- engine
+                               },
+                               get_engine = function() {
+                                 self$engine
+                               },
+                               get_params = function() {
+                                 self$params
+                               },
                                fit = function(X, y, ...) {
                                  self$X_train <- X
                                  self$y_train <- y
+                                 self$params <- list(...)
                                  self$set_model(stats::.lm.fit(x = self$X_train,
                                                                y = self$y_train,
                                                                ...))
+                                 self$set_engine(list(fit = stats::.lm.fit, 
+                                                      predict = function(X) drop(X%*%self$model$coefficients)))
                                  return(base::invisible(self))
                                },
                                predict = function(X, level = NULL,
                                                   method = c("splitconformal",
                                                              "other"),
                                                   ...) {
-                                 if (is.null(level))
+                                 
+                                 if (is.null(self$model) || is.null(self$engine))
+                                  stop(paste0(self$name, " must be fitted first"))
+                                 
+                                 if (is.null(level)) # no prediction interval
                                  {
-
-                                   return(drop(X%*%self$model$coefficients))
-
+                                   try_return <- try(return(self$engine$predict(X)), 
+                                       silent = TRUE)
+                                   if (inherits(try_return, "try-error"))
+                                   {
+                                     return(self$engine$predict(self$model, X))
+                                   }
                                  } else { # prediction interval
 
                                    method <- match.arg(method)
 
                                    if (identical(method, "splitconformal"))
                                    {
-                                     # see https://github.com/ryantibs/conformal
-                                     # see https://github.com/ryantibs/conformal
-                                     # see https://github.com/ryantibs/conformal
                                      stopifnot(is_wholenumber(level))
                                      stopifnot(level > 0 && level < 100)
                                      stopifnot(!is.null(self$X_train))
@@ -67,31 +89,31 @@ BaseRegressor <- R6::R6Class("BaseRegressor",
                                                                          p = 0.5,
                                                                          seed = 123)
 
-                                     X_train_sc <- self$X_train[idx_train_calibration,]
+                                     X_train_sc <- self$X_train[idx_train_calibration, ]
                                      y_train_sc <- self$y_train[idx_train_calibration]
-                                     X_calibration_sc <- self$X_train[-idx_train_calibration,]
+                                     X_calibration_sc <- self$X_train[-idx_train_calibration, ]
                                      y_calibration_sc <- self$y_train[-idx_train_calibration]
-
-                                     fit_obj_train_sc <- .lm.fit(X_train_sc, y_train_sc)
-                                     y_pred_calibration <- X_calibration_sc%*%fit_obj_train_sc$coefficients
+                                      
+                                     fit_obj_train_sc <- self$engine$fit(x = X_train_sc, y = y_train_sc)
+                                     y_pred_calibration <- try(self$engine$predict(X = X_calibration_sc), 
+                                                               silent = TRUE)
+                                     if (inherits(y_pred_calibration, "try-error"))
+                                     {
+                                       y_pred_calibration <- self$engine$predict(self$model, X_calibration_sc)
+                                     }
                                      abs_residuals <- abs(y_calibration_sc - y_pred_calibration)
-
-                                     # quantile_absolute_residuals_lower <- quantile_scp(abs_residuals,
-                                     #                                                   alpha = (1 - level / 100) / 2)
-                                     # quantile_absolute_residuals_upper <- quantile_scp(abs_residuals,
-                                     #                                                   alpha = 1 - (1 - level / 100) / 2)
-
-                                     # quantile_absolute_residuals_lower <- quantile(abs_residuals,
-                                     #                                               probs = (1 - level / 100) / 2)
-                                     # quantile_absolute_residuals_upper <- quantile(abs_residuals,
-                                     #                                               probs = 1 - (1 - level / 100) / 2)
 
                                      quantile_absolute_residuals_lower <- quantile_scp(abs_residuals,
                                                                                        alpha = (1 - level / 100))
                                      quantile_absolute_residuals_upper <- quantile_scp(abs_residuals,
                                                                                        alpha = (1 - level / 100))
-
-                                     preds <- drop(X%*%self$model$coefficients)
+                                     
+                                     preds <- try(return(self$engine$predict(X, ...)), 
+                                                       silent = TRUE)
+                                     if (inherits(preds, "try-error"))
+                                     {
+                                       preds <- self$engine$predict(self$model, X, ...)
+                                     }
 
                                      return(list(preds = preds,
                                                  lower = preds - quantile_absolute_residuals_lower,
@@ -116,10 +138,12 @@ BaseClassifier <- R6::R6Class(classname = "BaseClassifier",
                             model = NULL,
                             initialize = function(name = "BaseClassifier",
                                                   type = "classification",
-                                                  model = NULL) {
+                                                  model = NULL, 
+                                                  engine = NULL) {
                               self$name <- name
                               self$type <- type
                               self$model <- model
+                              self$engine <- engine
                             },
                             get_name = function() {
                               return(self$name)
@@ -132,4 +156,10 @@ BaseClassifier <- R6::R6Class(classname = "BaseClassifier",
                             },
                             set_model = function(model) {
                               self$model <- model
+                            },
+                            set_engine = function(engine) {
+                              self$engine <- engine
+                            },
+                            get_engine = function() {
+                              self$engine
                             }))
--- R/ranger.R
+++ R/ranger.R
@@ -4,46 +4,76 @@
 RangerRegressor <- R6::R6Class(classname = "RangerRegressor",
                                inherit = learningmachine::BaseRegressor,
                                public = list(
+                                 name = "RangerRegressor",
+                                 type = "regression",
+                                 model = NULL,
+                                 X_train = NULL,
+                                 y_train = NULL,
+                                 engine = NULL, 
+                                 params = NULL,
                                  initialize = function(name = "RangerRegressor",
-                                                       type = "regression"){
+                                                       type = "regression",
+                                                       model = NULL,
+                                                       X_train = NULL,
+                                                       y_train = NULL,
+                                                       engine = NULL, 
+                                                       params = NULL){
                                    self$name <- name
                                    self$type <- type
+                                   self$model <- model
+                                   self$X_train <- X_train
+                                   self$y_train <- y_train
+                                   self$engine <- engine
+                                   self$params <- params
                                  },
                                  fit = function(X, y, ...) {
                                    if(is_package_available("ranger") == FALSE)
-                                     install.packages("ranger")
+                                     install.packages("ranger",
+                                                      repos = c(CRAN = "https://cloud.r-project.org"))
+                                   self$X_train <- X
+                                   self$y_train <- y
+                                   self$params <- list(...)
                                    self$set_model(fit_func_ranger(x = X, y = y, ...))
+                                   self$set_engine(list(fit = fit_func_ranger, 
+                                                        predict = predict_func_ranger))
                                    return(base::invisible(self))
                                  },
-                                 predict = function(X) {
-                                   predict_func_ranger(obj = self$model,
-                                                       newx = X)
+                                 predict = function(X, level = NULL,
+                                                    method = c("splitconformal",
+                                                               "other"),
+                                                    ...) {
+                                   method <- match.arg(method)
+                                   super$predict(X = X, level = level,
+                                                 method = method)
                                  }
                                ))
 
-# predict_proba
 
 # 2 - RangerClassifier -------------------------------------------------------------------
 
 RangerClassifier <- R6::R6Class(classname = "RangerClassifier",
-                               inherit = learningmachine::BaseClassifier,
-                               public = list(
-                                 initialize = function(name = "RangerClassifier",
-                                                       type = "classification"){
-                                   self$name <- name
-                                   self$type <- type
-                                 },
-                                 fit = function(X, y, ...) {
-                                   if(is_package_available("ranger") == FALSE)
-                                     install.packages("ranger")
-                                   self$set_model(fit_func_ranger(x = X, y = y, ...))
-                                   return(base::invisible(self))
-                                 },
-                                 predict = function(X, type = "response") {
-                                   predict_func_ranger(self$model, newx = X,
-                                                       type = type)
-                                 }
-                               ))
+                                inherit = learningmachine::BaseClassifier,
+                                public = list(
+                                  initialize = function(name = "RangerClassifier",
+                                                        type = "classification",
+                                                        engine = NULL){
+                                    self$name <- name
+                                    self$type <- type
+                                    self$engine <- engine
+                                  },
+                                  fit = function(X, y, ...) {
+                                    if(is_package_available("ranger") == FALSE)
+                                      install.packages("ranger",
+                                                       repos = c(CRAN = "https://cloud.r-project.org"))
+                                    self$set_engine(fit_func_ranger)
+                                    self$set_model(fit_func_ranger(x = X, y = y, ...))
+                                    return(base::invisible(self))
+                                  },
+                                  predict = function(X, type = "response") {
+                                    predict_func_ranger(self$model, newx = X,
+                                                        type = type)
+                                  }
+                                ))
 
 # 3 - utils -------------------------------------------------------------------
 
