046a01499cc25470933296e5a6e9d26036749d3c
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-07-09 13:58:40 +0200

    install pkgs onload and update Depends

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,17 @@
+.onLoad <- function(libname, pkgname) {
+  
+  required_packages <- c("skimr", "bcn")
+  
+  ensure_package <- function(package_name) {
+    if (!requireNamespace(package_name, quietly = TRUE)) {
+      utils::install.packages(package_name, 
+                              repos = c('https://techtonique.r-universe.dev', 
+                                        'https://cloud.r-project.org'), 
+                              dependencies = TRUE)
+    }
+    library(package_name, character.only = TRUE)
+  }
+  
+  invisible(lapply(required_packages, ensure_package))
+}
 
